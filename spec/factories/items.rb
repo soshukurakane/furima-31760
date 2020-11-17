eb5b386e833c1121195b885bea758ec19d6892ff
soshukurakane <73089033+soshukurakane@users.noreply.github.com>
2020-11-17 12:22:07 +0900

    itemモデルの単体テストコード

---
+++ spec/factories/items.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :item do
+    title {"商品名"}
+    category_id {"5"}
+    status_id {"5"}
+    burden_id {"2"}
+    area_id {"3"}
+    day_id {"2"}
+    selling_price {"1000"} 
+    explanation {Faker::Lorem.sentence}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end  
+    association :user 
+  end
+end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  describe 'create' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    it 'すべてのカラムが存在すれば保存できる' do
+      expect(@item).to be_valid
+    end
+
+    it 'titleが空では保存できない' do
+      @item.title = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Title can't be blank")
+    end
+
+    it 'explanationが空では保存できない' do
+      @item.explanation = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Explanation can't be blank")
+    end
+    it 'category_idが空では保存できない' do
+      @item.category_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end
+    it 'statusが空では保存できない' do
+      @item.status_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status can't be blank")
+    end
+    it 'burdenが空では保存できない' do
+      @item.burden_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Burden can't be blank")
+    end
+    it 'areaが空では保存できない' do
+      @item.area_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Area can't be blank")
+    end
+    it 'dayが空では保存ができない' do
+      @item.day_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Day can't be blank")
+    end
+    it 'selling_priceが空では保存されない' do
+      @item.selling_price = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Selling price can't be blank")
+    end
+    it 'imageが空では保存されない' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+    it '指定の価格範囲を超えると保存できない' do
+      @item.selling_price = "280"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Selling price must be greater than 299")
+    end
+    it '価格は半角数字でないと保存されない' do
+      @item.selling_price = "４００"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Selling price is not a number")
+    end
+  end
+end
