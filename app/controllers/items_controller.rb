74ba72c5e285e65ea7e151ec0194de50cd448c45
soshukurakane <73089033+soshukurakane@users.noreply.github.com>
2020-11-18 15:47:56 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,8 +39,10 @@ class ItemsController < ApplicationController
 
   def destroy
     if current_user.id == @item.user_id
-      item.destroy
+      @item.destroy
       redirect_to root_path
+    else
+      render :show
     end
   end
 
